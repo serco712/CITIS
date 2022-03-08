cba75319b616d6a719ac386979a4061f4dd58ccf
serco712 <98959255+serco712@users.noreply.github.com>
2022-03-08 14:39:30 +0100

    Cambios
    Main

--- src/app/launcher/Main.java
+++ src/app/launcher/Main.java
@@ -1,8 +1,11 @@
 package app.launcher;
 
+import app.view.MainWindow;
+
 public class Main {
 
 	public static void main(String[] args) {
+		new MainWindow();
 	}
 
 }
--- src/app/view/MainWindow.java
+++ src/app/view/MainWindow.java
@@ -1,7 +1,32 @@
 package app.view;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class MainWindow extends JFrame {
 	
+	public MainWindow() {
+		super ("CITIS");
+		initGUI();
+	}
+	
+	private void initGUI() {
+		this.setSize(600, 600);
+		JPanel mainPanel = new JPanel();
+		JTextField jtf = new JTextField(20);
+		JPasswordField jpf = new JPasswordField(20);
+		
+		mainPanel.add(jtf);
+		mainPanel.add(jpf);
+		
+		this.add(mainPanel);
+		
+		
+		this.setVisible(true);
+	}
 }
