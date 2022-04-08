9f20cf637631f31421e43b9e76d0788bc3c67f51
serco712 <98959255+serco712@users.noreply.github.com>
2022-04-08 14:50:33 +0200

    Inicio de DAOs

--- src/app/model/layers/integration/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package app.model.layers.integration;
-
-import app.model.business.user.ASUser;
-
-public interface UserDAO {
-	public ASUser findUser(String id);
-	public void saveUser(ASUser user);
-	public ASUser createUser(ASUser user);
-}
---
+++ src/app/model/layers/integration/transport/TransportDatabaseDAO.java
@@ -0,0 +1,42 @@
+package app.model.layers.integration.transport;
+
+import java.sql.*;
+
+import app.model.business.transport.DTOTransport;
+
+public class TransportDatabaseDAO implements TransportDAO {
+
+	@Override
+	public DTOTransport findTransport(String id) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		DTOTransport transport = null;
+		
+		try {
+			con = getConnection();
+			ps = con.prepareStatement("SELECT * "
+									+ "FROM ")
+		}
+		
+		return null;
+	}
+
+	private Connection getConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveTransport(DTOTransport transport) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DTOTransport createTransport(DTOTransport transport) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
