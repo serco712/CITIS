c3203e3c28ea3d9be48518d46a7ab67bcd4d3e0d
serco712 <98959255+serco712@users.noreply.github.com>
2022-05-02 10:34:35 +0200

    DAOFactory
    Primera aproximacion al añadido del abstract factory

---
+++ src/app/model/layers/integration/factories/DAOFactoryImp.java
@@ -0,0 +1,48 @@
+package app.model.layers.integration.factories;
+
+import app.model.layers.integration.line.LineDAO;
+import app.model.layers.integration.line.LineDatabaseDAO;
+import app.model.layers.integration.specifictrip.SpecificTripDAO;
+import app.model.layers.integration.specifictrip.SpecificTripDatabaseDAO;
+import app.model.layers.integration.station.StationDAO;
+import app.model.layers.integration.station.StationDatabaseDAO;
+import app.model.layers.integration.transport.TransportDAO;
+import app.model.layers.integration.transport.TransportDatabaseDAO;
+import app.model.layers.integration.trip.TripDAO;
+import app.model.layers.integration.trip.TripDatabaseDAO;
+import app.model.layers.integration.user.UserDAO;
+import app.model.layers.integration.user.UserDatabaseDAO;
+
+public class DAOFactoryImp extends DAOFactory {
+
+	@Override
+	public LineDAO createLineDAO() {
+		return new LineDatabaseDAO();
+	}
+
+	@Override
+	public SpecificTripDAO createSpecificDAO() {
+		return new SpecificTripDatabaseDAO();
+	}
+
+	@Override
+	public StationDAO createStationDAO() {
+		return new StationDatabaseDAO();
+	}
+
+	@Override
+	public TransportDAO createTransportDAO() {
+		return new TransportDatabaseDAO();
+	}
+
+	@Override
+	public TripDAO createTripDAO() {
+		return new TripDatabaseDAO();
+	}
+
+	@Override
+	public UserDAO createUserDAO() {
+		return new UserDatabaseDAO();
+	}
+
+}
